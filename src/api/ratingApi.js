ca148673db03a456f205166cb42d8fdcd8fa68d6
Gonzalo <gonzalorom2001@gmail.com.com>
2025-05-15 17:54:44 -0300

    api2

--- src/api/ratingApi.js
+++ src/api/ratingApi.js
@@ -3,6 +3,6 @@ const api = axios.create({
   baseURL: `http://www.omdbapi.com/`
 })
 
-export const traeRating = (imdb) => api.get(`?apikey=dbb5dbf7&i=${imdb}`)
-// export const traeRating = (imdb) => api.get(`?apikey=${import.meta.env.VITE_OMDB_APIKEY}&i=${imdb}`)
+// export const traeRating = (imdb) => api.get(`?apikey=dbb5dbf7&i=${imdb}`)
+export const traeRating = (imdb) => api.get(`?apikey=${import.meta.env.VITE_OMDB_APIKEY}&i=${imdb}`)
 
